3b053822aa4935571148d943bcb8e53eebb55420
Abdulshakur <mabdulshakur54@gmail.com>
2025-05-08 18:04:54 +0100

    migrated default migrations
    created api and account app
    
    created serializer, view and url for user registration using the default user model from django.contrib.auth

---
+++ account/views.py
@@ -0,0 +1,9 @@
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+
+class RegistrationView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
